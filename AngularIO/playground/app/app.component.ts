ef7b65405c3d3058f0bf75741874b7f9a9df1b98
exp2018 <35161785+exp2018@users.noreply.github.com>
2018-01-09 16:03:59 +0200

    Service inject

--- AngularIO/playground/app/app.component.ts
+++ AngularIO/playground/app/app.component.ts
@@ -1,13 +1,5 @@
-
-import { Component } from '@angular/core'
-
-const CONTACTS: Contact[] = [
-    { id: 1, firstName: "Max", lastName: "Smith", email: "max@gmail.com" },
-    { id: 2, firstName: "Chris", lastName: "Raches", email: "chris@gmail.com" },
-    { id: 3, firstName: "Michael", lastName: "Alloy", email: "michael@gmail.com" },
-    { id: 4, firstName: "John", lastName: "Doe", email: "john@gmail.com" },
-    { id: 5, firstName: "Jenny", lastName: "Doe", email: "jenny@gmail.com" }
-  ];
+import { ContactsService } from './contact.service';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
     selector: 'my-app',
@@ -15,19 +7,33 @@ const CONTACTS: Contact[] = [
         <ul>
                 <li *ngFor="let contact of contacts" class='item' [class.active]="selected==contact"> 
                     <a href='#' (click)='select(contact)'>{{contact.firstName}} {{contact.lastName.toUpperCase()}}</a>
-                    <a href='#' onclick='ctrl.remove(event, 1)' class='remove' title='Remove'><span class='glyphicon glyphicon-remove-sign'></span></a>
+                    <a href='#' (click)='remove(contact)' class='remove' title='Remove'><span class='glyphicon glyphicon-remove-sign'></span></a>
                 </li>
         </ul>
-        <div id="contactsDetailsContainer">
+        <div id="contactsDetailsContainer" *ngIf="selected">
+            <label>First Name: </label><b>{{selected.firstName}}</b><br/>
+            <label>Last Name: </label><b>{{selected.lastName}}</b><br/>
+            <label>email: </label><b>{{selected.email}}</b><br/>
+            <label></label><a href="#" class="text-danger" onclick="ctrl.edit(event, 1)"><span class="glyphicon glyphicon-edit"></span>Edit</a><br/>
         </div>
         <pre>{{selected | json}}</pre>
     `
 })
-export class AppComponent {
-    contacts = CONTACTS
+export class AppComponent implements OnInit {
+    contacts: Contacts
     selected: Contact
+
+    constructor(private contactService: ContactsService) {}
+
+    ngOnInit() {
+        this.contacts = this.contactService.getAll()
+    }
 
     select(contact: Contact) {
         this.selected = contact
+    }
+
+    remove(contact: Contact): void {
+        this.contactService.remove(contact.id)
     }
 }
--- AngularIO/playground/app/app.module.ts
+++ AngularIO/playground/app/app.module.ts
@@ -1,11 +1,12 @@
-
 import { NgModule }                 from '@angular/core'
 import { BrowserModule }            from '@angular/platform-browser'
 import { AppComponent }             from './app.component'
+import { ContactsService }          from './contact.service'
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers:    [ContactsService]
 })
 export class AppModule {}
