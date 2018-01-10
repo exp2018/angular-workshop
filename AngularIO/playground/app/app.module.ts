4e0f7f12437af6379f2e85acacff763c96641dc1
exp2018 <35161785+exp2018@users.noreply.github.com>
2018-01-10 17:10:36 +0200

    fix path

--- AngularIO/playground/app/app.module.ts
+++ AngularIO/playground/app/app.module.ts
@@ -1,15 +1,15 @@
 import { FormsModule }              from '@angular/forms';
-import { ContactDetailsComponent }  from './Contacts/contact-details.component';
+import { ContactDetailsComponent }  from './contacts/contact-details.component';
 import { NgModule }                 from '@angular/core'
 import { HttpModule }               from '@angular/http'
 import { BrowserModule }            from '@angular/platform-browser'
 import { AppComponent }             from './app.component'
-import { ContactsService }          from './Contacts/contact.service'
-import { ContactListComponent }     from './Contacts/contact-list.component';
+import { ContactsService }          from './contacts/contact.service'
+import { ContactListComponent }     from './contacts/contact-list.component';
 import { EmailValidator }           from './email-validator.directive';
-import { AboutComponent }           from './About/about.component'
+import { AboutComponent }           from './about/about.component'
 import { RouterModule, Route, Routes } from '@angular/router'
-import { ContactsComponent }        from './Contacts/contacts.component';
+import { ContactsComponent }        from './contacts/contacts.component';
 
 const routes: Routes = [
   { path: 'contacts',       component: ContactsComponent },
